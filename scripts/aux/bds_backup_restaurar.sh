1c3fb32d35e8cd8ffab7d725a59c0891bd62b3d3
Fernando Pirani <feremp.i+dev@gmail.com>
2017-09-22 04:47:38 -0300

    Manejo de bases.ini  - Ahora se maneja el archivo bases.ini

--- scripts/aux/bds_backup_restaurar.sh
+++ scripts/aux/bds_backup_restaurar.sh
@@ -12,14 +12,25 @@ HOST="pg";
 ARCHIVO="backup"
 ARCHIVO_TOBA="backup_toba"
 
-set -x
-gzip -d $DIRBK/$ARCHIVO.backup.gz
-$DIRBD/bd_app_crear.sh
-psql -h $HOST -U postgres --set ON_ERROR_STOP=off $BASEDEDATOS < $DIRBK/$ARCHIVO.backup
-gzip $DIRBK/$ARCHIVO.backup
+ARCHIVO_BACKUP=$DIRBK/$ARCHIVO.backup
+ARCHIVO_BACKUP_TOBA=$DIRBK/$ARCHIVO_TOBA.backup
 
-gzip -d $DIRBK/$ARCHIVO_TOBA.backup.gz
-$DIRBD/bd_toba_crear.sh
-psql -h $HOST -U postgres --set ON_ERROR_STOP=off $BD_TOBA < $DIRBK/$ARCHIVO_TOBA.backup
-gzip $DIRBK/$ARCHIVO_TOBA.backup
-set +x
+if [ -f "$ARCHIVO_BACKUP.gz" ];
+then
+  set -x
+  gzip -d "$ARCHIVO_BACKUP.gz"
+  $DIRBD/bd_app_crear.sh
+  psql -h $HOST -U postgres --set ON_ERROR_STOP=off $BASEDEDATOS < $ARCHIVO_BACKUP
+  gzip $ARCHIVO_BACKUP
+  set +x
+fi
+
+if [ -f "$ARCHIVO_BACKUP_TOBA.gz" ];
+then
+  set -x
+  gzip -d "$ARCHIVO_BACKUP_TOBA.gz"
+  $DIRBD/bd_toba_crear.sh
+  psql -h $HOST -U postgres --set ON_ERROR_STOP=off $BD_TOBA < $ARCHIVO_BACKUP_TOBA
+  gzip $ARCHIVO_BACKUP_TOBA
+  set +x
+fi
